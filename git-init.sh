1610937e70ea78fa8aacd1bccd521209dd2f5a8d
fgwsz <3191000458@qq.com>
2024-07-28 09:41:20 +0800

    add rm .git

--- git-init.sh
+++ git-init.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+root_path=$(dirname "$(readlink -f "$0")")
+git_path="$root_path/.git"
 read -p "Input Git Repo Name: " repo_name
 echo "# $repo_name" >> README.md
+if [ -d "$git_path" ]; then
+    rm -rf "$git_path"
+fi
 git init
 git add README.md
 git commit -m "first commit"
