49a22f4d743b92362a9c97c7aed43fef9f468904
fgwsz <3191000458@qq.com>
2024-07-28 09:56:33 +0800

    rewrite readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# linux-cpp-repo
+linux操作系统上的`C++`项目`git`仓库模板
+
+`source`目录用于存放`C++`头文件和源文件
+
+`build.sh`生成`build`目录和可执行文件,并运行生成的可执行文件
+
+`clean.sh`清理`build`目录
+
+`git-init.sh`初始化`git`仓库
+
+`git-push.sh`提交到`git`仓库
--- git-init.sh
+++ git-init.sh
@@ -2,7 +2,7 @@
 root_path=$(dirname "$(readlink -f "$0")")
 git_path="$root_path/.git"
 read -p "Input Git Repo Name: " repo_name
-echo "# $repo_name" >> README.md
+echo "# $repo_name" > README.md
 if [ -d "$git_path" ]; then
     rm -rf "$git_path"
 fi
