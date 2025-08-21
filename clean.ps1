0fde7055e47f9ebec83e5d36578261e617a34f4c
fgwsz <3191000458@qq.com>
2025-08-21 11:20:44 +0800

    fix clean

--- clean.ps1
+++ clean.ps1
@@ -1,9 +1,13 @@
 $root_path=Split-Path -Parent $MyInvocation.MyCommand.Definition
 $build_path=Join-Path $root_path "build/windows"
-$binary_path=Join-Path $root_path "binary.exe"
+$binary_debug_path=Join-Path $root_path "binary-debug.exe"
+$binary_release_path=Join-Path $root_path "binary-release.exe"
 if(Test-Path -Path $build_path){
     Remove-Item $build_path -Recurse -Force
 }
-if(Test-Path -Path $binary_path){
-    Remove-Item $binary_path -Recurse -Force
+if(Test-Path -Path $binary_debug_path){
+    Remove-Item $binary_debug_path -Recurse -Force
+}
+if(Test-Path -Path $binary_release_path){
+    Remove-Item $binary_release_path -Recurse -Force
 }
--- clean.sh
+++ clean.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 root_path=$(dirname "$(readlink -f "$0")")
 build_path="$root_path/build/linux"
-binary_path="$root_path/binary"
+binary_debug_path="$root_path/binary-debug"
+binary_release_path="$root_path/binary-debug"
 if [ -d "$build_path" ]; then
     rm -rf "$build_path"
 fi
-if [ -e "$binary_path" ]; then
-    rm -rf "$binary_path"
+if [ -e "$binary_debug_path" ]; then
+    rm -rf "$binary_debug_path"
+fi
+if [ -e "$binary_release_path" ]; then
+    rm -rf "$binary_release_path"
 fi
